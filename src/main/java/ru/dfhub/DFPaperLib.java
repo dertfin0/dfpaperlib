86e8dc34c35b30f4593b40c7bfc7a50b7ad56899
Dertfin3051 <dertfin@gmail.com>
2024-12-15 22:58:59 +0300

    1.2

---
+++ src/main/java/ru/dfhub/DFPaperLib.java
@@ -0,0 +1,8 @@
+package ru.dfhub;
+
+public class DFPaperLib {
+
+    public static void enable() {
+        System.out.println("dfpaperlib enabled!");
+    }
+}
--- src/main/java/ru/dfhub/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.dfhub;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("dfpaperlib started");
-    }
-}
